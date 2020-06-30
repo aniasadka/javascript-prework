c51c6b957f0b9eb1490ae013d7b87e4673b19629
Anna Sadka <anna.sadka87@gmail.com>
2020-06-30 12:20:52 +0200

    add last task

--- js/script.js
+++ js/script.js
@@ -22,19 +22,6 @@ printMessage('Mój ruch to: ' + computerMove);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -54,3 +41,44 @@ if(playerInput == '3') {
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+
+else( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('przegrywasz!');
+  }
+
+else( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Jest remis!');
+  }
+
+
+else( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ty przegrywasz!');
+}
+
+else( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}
+
+else( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Jest remis!');
+}
+
+
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ty przegrywasz!');
+}
+
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}
+
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Jest remis!');
+}
